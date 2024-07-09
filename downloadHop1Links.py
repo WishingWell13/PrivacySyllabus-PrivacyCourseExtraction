09d4f876f7ec9e5ec4f1d65b81ec4f06a85ebd32
Andrew Smithwick <andy.smithwick@gmail.com>
2024-07-08 21:01:23 -0700

    Removed ignore index

--- downloadHop1Links.py
+++ downloadHop1Links.py
@@ -124,7 +124,7 @@ def exit_handler():
     # dfTooManyRequests.to_csv(storageLocation + 'tooManyRequestUniversities.csv', index = False)
     # dfBadCertificate.to_csv(storageLocation + 'badCertificateUniversities.csv', index = False)
     
-    dfGeneralErrors = pd.DataFrame(dfGeneralErrorsList, ignore_index=True)
+    dfGeneralErrors = pd.DataFrame(dfGeneralErrorsList)
     dfGeneralErrors.to_csv(storageLocation + 'generalErrorUniversities.csv', index = False)
 
 def kill_handler(*args):
@@ -177,7 +177,7 @@ for university, process in processes:
         # dfTooManyRequests.to_csv(storageLocation + 'tooManyRequestUniversities.csv', index = False)
         # dfBadCertificate.to_csv(storageLocation + 'badCertificateUniversities.csv', index = False)
         
-        dfGeneralErrors = pd.DataFrame(dfGeneralErrorsList, ignore_index=True)
+        dfGeneralErrors = pd.DataFrame(dfGeneralErrorsList)
         dfGeneralErrors.to_csv(storageLocation + 'generalErrorUniversities.csv', index = False)
 
 print(results)
@@ -193,7 +193,7 @@ dfTimeout.to_csv(storageLocation + 'TimeoutUniversities.csv', index = False)
 # dfTooManyRequests.to_csv(storageLocation + 'tooManyRequestUniversities.csv', index = False)
 # dfBadCertificate.to_csv(storageLocation + 'badCertificateUniversities.csv', index = False)
 
-dfGeneralErrors = pd.DataFrame(dfGeneralErrorsList, ignore_index=True)
+dfGeneralErrors = pd.DataFrame(dfGeneralErrorsList)
 dfGeneralErrors.to_csv(storageLocation + 'generalErrorUniversities.csv', index = False)
 
 # Close and join the pool
